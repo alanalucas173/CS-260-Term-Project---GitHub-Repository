4bd78af8c706bcb25be96bdab16138ee17a94bc4
Maximus662 <164969577+Maximus662@users.noreply.github.com>
2024-04-05 11:53:26 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,25 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <cctype>
 
 using namespace std;
 
+//This takes customer information (name, address, email, phone)
 class Customer {
 private:
-	string fname, lname, address, email; 
+	string fname, lname, address, email;
 	double phone;
 public:
+	//default
 	Customer()
 	{
 		fname = "";
 		lname = "";
 		address = "";
 		email = "";
-		phone = "";
+		phone = 0;
 	}
 	Customer(string _fname, string _lname, string _address, string _email, double _phone)
 	{
@@ -32,7 +37,8 @@ public:
 		email = _email;
 		phone = _phone;
 	}
-	
+
+
 	//Setters
 	void setFname(string _fname)
 	{
@@ -96,38 +102,103 @@ public:
 	}
 };
 
-class Account : public Customer{
+//makes an account(ID and balance) with the customers information
+//also withdraw and deposit 
+//has Customer accountCustomer
+class Account : public Customer {
 private:
 	int ID;
 	double balance;
 	static int withdrawals;
 	static int deposits;
+protected:
 	Customer accountCustomer;
 
 public:
 	//default
-	Account():Customer()
+	Account() :Customer()
 	{
 		ID = 0;
 		balance = 0;
 	}
 
-	Account(int ID, double balance, Customer accountCustomer) : Customer (string _fname, string _lname, string _address, string _email, double _phone)
+	Account(string _fname, string _lname, string _address, string _email, double _phone, int _ID, double _balance) : Customer(_fname, _lname, _address, _email, _phone)
 	{
-		ID = 0;
-		balance = 0;
+		if(isdigit(_ID) && _ID > 0)
+			ID = _ID;
+		else
+		{
+			while (isalpha(_ID) || _ID <= 0)
+			{
+				cout << "You have entered an invalid character or number for ID number. Please enter a new ID number: ";
+				cin >> _ID;
+			}
+		}
+		if(isdigit(_balance) && _balance > 0)
+		balance = _balance;
+		else
+		{
+			while (isalpha(_balance) || _balance <= 0)
+			{
+				cout << "You have entered an invalid character or number for ID number. Please enter a new ID number: ";
+				cin >> _balance;
+			}
+		}
 		//accountCustomer.setCustomer() idr how to do this
 	}
-	Account
-
+	//setter
+	void setAccount(string _fname, string _lname, string _address, string _email, double _phone, int _ID, double _balance)
+	{
+		setCustomer(_fname, _lname, _address, _email, _phone);
+		if (isdigit(_ID) && _ID > 0)
+			ID = _ID;
+		else
+		{
+			while (isalpha(_ID) || _ID <= 0)
+			{
+				cout << "You have entered an invalid character or number for ID number. Please enter a new ID number: ";
+				cin >> _ID;
+			}
+		}
+		if (isdigit(_balance) && _balance > 0)
+			balance = _balance;
+		else
+		{
+			while (isalpha(_balance) || _balance <= 0)
+			{
+				cout << "You have entered an invalid character or number for ID number. Please enter a new ID number: ";
+				cin >> _balance;
+			}
+		}
+	}
+	void setBalance(double _balance)
+	{
+		if (isdigit(_balance) && _balance > 0)
+			balance = _balance;
+		else
+		{
+			while (isalpha(_balance) || _balance <= 0)
+			{
+				cout << "You have entered an invalid character or number for ID number. Please enter a new ID number: ";
+				cin >> _balance;
+			}
+		}
+	}
+	//getter
+	double getBalance() const
+	{
+		return balance;
+	}
 	//Functions
 	void withdrawal(double _withdrawalAmmount)
 	{
-		if (_withdrawalAmmount < 0 )
+		//checks to make sure not below 0
+		if (_withdrawalAmmount < 0)
 		{
 			cout << "Not a valid withdrawal ammount, please try again... " << endl;
 			cout << "=========================================================================================================" << endl << endl;
 		}
+		//checks to make sure they have enough money 
 		else if ((balance - _withdrawalAmmount) < 0)
 		{
 			cout << "The withdrawal ammount was greater than your balance. Please try again with a lesser value..." << endl;
@@ -143,6 +214,7 @@ public:
 	}
 	void deposit(double _depositAmmount)
 	{
+		//checks to make sure amount is above 0
 		if (_depositAmmount < 0)
 		{
 			cout << "Not a valid deposit ammount, please try again... " << endl;
@@ -158,39 +230,293 @@ public:
 	}
 	void printInfo()
 	{
-		Customer : printInfo();
-		cout << "Account ID Number: " << ID << endl;
-		cout << "Balance: $" << balance << endl;
-		cout << "Number of withdrawals: " << withdrawals << endl;
-		cout << "Number of deposits: " << deposits << endl;
-	}
-};
-int Account :: withdrawals = 0;
-int Account :: deposits = 0;
+	Customer: printInfo();
 
+		cout << "Name" << setw(20) << "Account ID Number" << setw(20) << "Balance" << setw(20) << "Withdrawwals" << setw(20) << "Deposits" << endl;
+		cout << getFname << " " << getLname << setw(20) << ID << setw(20) << balance << setw(20) << withdrawals << setw(20) << deposits << endl;
+	}
+
+};
+//whats this for?
+int Account::withdrawals = 0;
+int Account::deposits = 0;
+
+//pulls from account  
 class CheckingAccount : public Account {
 private:
 	double overDraftLimit;
+
 public:
-	CheckingAccount(double overDraftLimit)
+	CheckingAccount() :Account()
+	{
+		overDraftLimit = 0;
+
+	}
+	CheckingAccount(string _fname, string _lname, string _address, string _email, double _phone, int _ID, double _balance, double _overDraftLimit) : Account(_fname, _lname, _address, _email, _phone, _ID, _balance)
+	{
+		overDraftLimit = _overDraftLimit;
+	}
+	void setOverDraftLimit(double _overDraftLimit)
+	{
+		overDraftLimit = _overDraftLimit;
+	}
+	//not sure how to overload base class 
+	void withdrawals(double _withdrawalAmount) override
+	{
+		if ((getBalance()) < 0)
+		{
+			double temp;
+			if (_withdrawalAmount > overDraftLimit)
+			{
+				cout << "You have tried to withdraw more then your over draft limit allows you to..." << endl;
+				cout << "Please try again!" << endl;
+			}
+			else
+			{
+				temp = overDraftLimit - _withdrawalAmount;
+				setBalance((getBalance()) - 20);
+			}
+		}
+		else if (_withdrawalAmount < 0)
+		{
+			cout << "You have tried to withdraw a negative amount. You can not do this..." << endl;
+			cout << "Please try again!" << endl;
+		}
+		else if (((getBalance()) - _withdrawalAmount) > 0)
+		{
+			double temp;
+			temp = getBalance() - _withdrawalAmount;
+			setBalance(temp);
+			withdrawals++;
+			cout << "Your inputted amount has been withdrawn... " << endl;
+			cout << "Your new balance is $" << temp << endl;
+		}
+		else if (((Account:: getBalance()) - _withdrawalAmount) < 0 && ((Account:: getBalance()) - _withdrawalAmount) >= (0 - overDraftLimit))
+		{
+			double temp;
+			temp = (overDraftLimit)+(getBalance() - (_withdrawalAmount));
+			setOverDraftLimit(temp);
+			setBalance((getBalance() - 20));
+			withdrawals++;
+			cout << "Your inputted amount has been withdrawn but a $20 service fee has been charged to your account for overdraft protection." << endl;
+		}
+		else
+		{
+			cout << "You have tried to withdraw more than your over draft limit allows you to." << endl;
+			cout << "Please try again." << endl;
+		}
+	}
 };
 
-int main()
+class SavingAccount : public Account {
+private:
+	double interestRate;
+public:
+	SavingAccount() :Account()
+	{
+		interestRate = 0;
+	}
+	SavingAccount(string _fname, string _lname, string _address, string _email, double _phone, int _ID, double _balance, double _interestRate)
+	{
+		accountCustomer.setCustomer(_fname, _lname, _address, _email, _phone);
+
+		interestRate = _interestRate;
+	}
+	double payInterest()
+	{
+		//somehow get that balance from Account 
+		// 
+		//double interest = interestRate * Account:getBalance();
+	}
+};
+//
+
+class menu : public Account
 {
+public:
+	int input;
+	void displayMenu()
+	{
+		cout << "Please choose from the following options:" << endl;
+		cout << "1* Deposit" << endl << "2* Withdraw" << endl << "3* Transfer Money" << endl << "4* Edit Info" << endl << "5* View Account Info" << endl << "6* Close Account " << endl << "Press any other number to exit" << endl;
+		cin >> input;
+	}
+
+};
+
+class createAccount : public Account
+{
+	static void displayMainMenu() {
+		cout << "1. Create Checking Account" << endl;
+		cout << "2. Create Saving Account" << endl;
+		cout << "3. Exit" << endl;
+	}
+	static int getUserChoice() {
+		int choice;
+		cout << "Enter your choice: ";
+		cin >> choice;
+		return choice;
+	}
+	static void createCheckingAccount() {
+		int id, phone;
+		double bal;
+		string fName, lName, addr, email;
+
+		cout << "Enter Account ID: ";
+		cin >> id;
+		cout << "Enter Initial Balance: ";
+		cin >> bal;
+		cout << "Enter First Name: ";
+		cin >> fName;
+		cout << "Enter Last Name: ";
+		cin >> lName;
+		cout << "Enter Address: ";
+		cin.ignore();
+		getline(cin, addr);
+		cout << "Enter Phone: ";
+		cin >> phone;
+		cout << "Enter Email: ";
+		cin >> email;
 
 
+		Account customer(fName, lName, addr, email, phone, id, bal);
+		//Account* acc = CreateAccount::createCheckingAccount(id, bal, customer);
+		cout << "Checking Account Created Successfully!" << endl;
+		//acc->PrintInfo();
+		//delete acc;
+	}
+
+	static void createSavingAccount() {
+		int id, phone;
+		double bal, rate;
+		string fName, lName, addr, email;
+
+		cout << "Enter Account ID: ";
+		cin >> id;
+		cout << "Enter Initial Balance: ";
+		cin >> bal;
+		cout << "Enter Interest Rate: ";
+		cin >> rate;
+		cout << "Enter First Name: ";
+		cin >> fName;
+		cout << "Enter Last Name: ";
+		cin >> lName;
+		cout << "Enter Address: ";
+		cin.ignore();
+		getline(cin, addr);
+		cout << "Enter Phone: ";
+		cin >> phone;
+		cout << "Enter Email: ";
+		cin >> email;
+
+		Account customer(fName, lName, addr, email, phone, id, bal);
+		//Account* acc = CreateAccount::createSavingAccount(id, bal, customer, rate);
+		cout << "Saving Account Created Successfully!" << endl;
+		//acc->PrintInfo();
+		//delete acc;
+	}
+	//creates a username and password
+	void registerUser(const string& username, const string& password) {
+		// Save to file (username.txt)
+		string filename = username + ".txt";
+		ofstream file(filename);
+		file << password << "\n";  // Save the password directly
+		file.close();
+
+		cout << "User registered successfully!" << endl;
+	}
+	// password and user for login
+	bool validateLogin(const string& username, const string& password) {
+		string filename = username + ".txt";
+		ifstream file(filename);
+		if (!file) {
+			cout << "User not found." << endl;
+			return false;
+		}
+
+	}
+};
+	int main()
+	{
+		//string _fname, string _lname, string _address, string _email, double _phone, int _ID, double _balance
+		// array for number of accounts 
+		Account account[20];
+
+		//Account account("alana", "lucas", "3547 Shres Dr", "alana@gmail", 9045984902, 1, 100);
+		//hard code an account
+		account[0].setAccount("alana", "lucas", "3547 Shres Dr", "alana@gmail", 9045984902, 1, 100);
+		account[1].setAccount("maximus", "adversalo", "736 Killebrew Way", "adversalom@gmail", 9165450572, 2, 1000);
+
+		int option;
+		cout << "1. Log in " << endl;
+		cout << "2. Create account" << endl;
+		//using files????
+		// we can add accounts username and password to save account information 
+		cin >> option;
+		if (option != 1 || 2)
+		{
+			cout << "Not a valid option" << endl;
+			cout << "Log in (L) or Create account (C)";
+		}
+		else if (option == 1)
+		{
+			//create class to log in (inherate from account)
+			string storedPassword, password;
+			getline(cin, storedPassword);
+
+			if (password == storedPassword) {
+				cout << "Login successful!" << endl;
+				return true;
+			}
+			else {
+				cout << "Invalid credentials." << endl;
+				return false;
+			}
+		}
+		else if (option == 2)
+		{
+			//create class to create accputn 
+			string username, password;
+			cout << "Enter username: ";
+			cin >> username;
+			cout << "Enter password: ";
+			cin >> password;
+
+			registerUser(username, password);
+		}
+
+		//create menu or call menu 
+		cout << "1. Select Saving Account" << endl;
+		cout << "2. Select Checking Account" << endl;
+		cout << "3. Exit" << endl;
+		cin >> option;
+
+		//either create options to select saving withdraw or cheicking or create a do while to display  
+		if (option == 1)
+		{
+			//call saving account
+			// create menu with options 
+			//deposit, withdraw, view account,  
+		}
+		else if (option == 2)
+		{
+			//call checking
+			// create menu with options 
+			//deposit, withdraw, view account
+		}
+		else if (option == 3)
+		{
+			//exits
+			cout << "Thank you" << endl;
+			return 0;
+		}
+		else
+		{
+			cout << "invalid option" << endl;
+		}
 
 
+		system("Pause");
+		return 0;
 
-
-
-
-
-
-
-
-
-
-	system("Pause");
-	return 0;
-}
+	}
